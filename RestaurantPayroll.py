f2184455422f3ced6e77f384d3070e19b507871c
kyle627 <kyleevangelisto@gmail.com>
2020-04-25 02:51:42 -0400

    fixed list and main method

--- RestaurantPayroll.py
+++ RestaurantPayroll.py
@@ -15,24 +15,22 @@ from RestaurantWorker import KitchenStaff
 from RestaurantWorker import HourlyWorker
 
 
-def main():
-    # Employees
+class RestaurantPayroll:
 
-    bussBoy = HourlyWorker("Sean McKay", "202-555-0102", "D", 20)
-    waiterA = WaitStaff("Steve Zoyac", "202-555-0103", "B", 30, 150, 25)
-    waiterB = WaitStaff("David Witte", "202-555-0104", "D", 30, 140, 25)
-    waiterC = WaitStaff("Robert MacAuley", "202-555-0105", "L", 30, 145, 25)
-    chefA = KitchenStaff("Kyle Evangelisto", "202-555-0106", "D", 40)
-    chefB = KitchenStaff("Angel Avelar", "202-555-0107", "L", 50)
-    chefC = KitchenStaff("Aidan Murphey", "202-555-0108", "D", 40)
-    chefD = KitchenStaff("Enrico Duller", "202-555-0109", "B", 55)
-
-    list = [bussBoy, waiterA, waiterB, waiterC, chefA, chefB, chefC, chefD]  # Employees in a list called list
-
-    # For every worker , print out their paycheck
-
-    for element in list:
-        element.generatePayCheck()
+    def main(self):
+        list = {  # Employees
+            HourlyWorker("Sean McKay", "202-555-0102", "D", 20),
+            WaitStaff("Steve Zoyac", "202-555-0103", "B", 30, 150, 25),
+            WaitStaff("David Witte", "202-555-0104", "D", 30, 140, 25),
+            WaitStaff("Robert MacAuley", "202-555-0105", "L", 30, 145, 25),
+            KitchenStaff("Kyle Evangelisto", "202-555-0106", "D", 40),
+            KitchenStaff("Angel Avelar", "202-555-0107", "L", 50),
+            KitchenStaff("Aidan Murphey", "202-555-0108", "D", 40),
+            KitchenStaff("Enrico Duller", "202-555-0109", "B", 55)
+        }
+        for element in list:  # For every worker , print out their paycheck
+            element.generatePayCheck()
 
 
-main()
+startup = RestaurantPayroll
+startup.main(startup)
